3e6b53f1407e4a1676b2cba0969ea08400b842c8
mendoza000 <omarmendoza.alv25@gmail.com>
2024-04-13 10:39:38 -0400

    add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+
+const queryClient = new QueryClient()
+
 function App() {
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
 			<p className="text-4xl">Hola</p>
-		</>
+		</QueryClientProvider>
 	)
 }
 

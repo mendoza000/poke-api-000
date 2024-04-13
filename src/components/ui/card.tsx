1e2857f5adfcf3c7d9085c487fd93d9b4ee1bf00
mendoza000 <omarmendoza.alv25@gmail.com>
2024-04-13 19:31:19 -0400

    separate conditionals class

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -12,8 +12,57 @@ export default function Card(props: Props) {
 		queryFn: () => fetch(props.url).then((res) => res.json()),
 	})
 
+	const colors = {
+		bg:
+			query.data?.types[0].type.name === "fire"
+				? "bg-red-400"
+				: query.data?.types[0].type.name === "water"
+				? "bg-blue-400"
+				: query.data?.types[0].type.name === "bug"
+				? "bg-yellow-600"
+				: "bg-green-700",
+
+		border:
+			query.data?.types[0].type.name === "fire"
+				? "border-red-400"
+				: query.data?.types[0].type.name === "water"
+				? "border-blue-400"
+				: query.data?.types[0].type.name === "bug"
+				? "border-yellow-600"
+				: "border-green-700",
+
+		hoverBorder:
+			query.data?.types[0].type.name === "fire"
+				? "hover:border-red-400"
+				: query.data?.types[0].type.name === "water"
+				? "hover:border-blue-400"
+				: query.data?.types[0].type.name === "bug"
+				? "hover:border-yellow-600"
+				: "hover:border-green-700",
+
+		bgType:
+			query.data?.types[0].type.name === "fire"
+				? "border-red-400"
+				: query.data?.types[0].type.name === "water"
+				? "border-blue-400"
+				: query.data?.types[0].type.name === "bug"
+				? "border-yellow-600"
+				: "border-green-700",
+
+		bgBlur:
+			query.data?.types[0].type.name === "fire"
+				? "bg-red-700/50"
+				: query.data?.types[0].type.name === "water"
+				? "bg-blue-700/50"
+				: query.data?.types[0].type.name === "bug"
+				? "bg-yellow-700/50"
+				: "bg-green-700/50",
+	}
+
 	return (
-		<div className="bg-gradient-to-b from-zinc-800  to-zinc-700 md:min-h-[18rem] rounded-xl px-3 pb-5 shadow-xl relative overflow-hidden flex flex-col items-center group mx-auto">
+		<div
+			className={`cursor-pointer bg-gradient-to-b from-zinc-800  to-zinc-900 md:min-h-[18rem] rounded-xl px-3 pb-5 shadow-xl relative overflow-hidden flex flex-col items-center group mx-auto border-2 border-transparent duration-200 ${colors.hoverBorder}`}
+		>
 			<img
 				src={query.data?.sprites.front_default}
 				alt={props.name}
@@ -27,15 +76,7 @@ export default function Card(props: Props) {
 			<p
 				className={`
           px-3 py-1 rounded-lg z-20 mt-2 capitalize min-w-[6rem]  text-center font-semibold flex items-center justify-center gap-1 text-white
-        ${
-					query.data?.types[0].type.name === "fire"
-						? "bg-red-400"
-						: query.data?.types[0].type.name === "water"
-						? "bg-blue-400"
-						: query.data?.types[0].type.name === "bug"
-						? "bg-yellow-600"
-						: "bg-green-700"
-				}`}
+        ${colors.bg}`}
 			>
 				{query.data?.types[0].type.name === "fire" && (
 					<IconFlame className="w-5 h-5" />
@@ -53,44 +94,16 @@ export default function Card(props: Props) {
 			</p>
 
 			<div className="z-20 flex gap-5 mt-3 font-semibold text-white uppercase">
-				<p
-					className={`border-b-2 ${
-						query.data?.types[0].type.name === "fire"
-							? "border-red-400"
-							: query.data?.types[0].type.name === "water"
-							? "border-blue-400"
-							: query.data?.types[0].type.name === "bug"
-							? "border-yellow-600"
-							: "border-green-700"
-					}`}
-				>
+				<p className={`border-b-2 ${colors.border}`}>
 					{query.data?.height / 10}mts
 				</p>
-				<p
-					className={`border-b-2 ${
-						query.data?.types[0].type.name === "fire"
-							? "border-red-400"
-							: query.data?.types[0].type.name === "water"
-							? "border-blue-400"
-							: query.data?.types[0].type.name === "bug"
-							? "border-yellow-600"
-							: "border-green-700"
-					}`}
-				>
+				<p className={`border-b-2 ${colors.bgType}`}>
 					{query.data?.weight / 10}kgs
 				</p>
 			</div>
 
 			<div
-				className={`h-[7rem] w-[13rem]  -bottom-20 left-1/2 -translate-x-1/2 absolute blur-3xl group-hover:h-[20rem] duration-200 ${
-					query.data?.types[0].type.name === "fire"
-						? "bg-red-700/50"
-						: query.data?.types[0].type.name === "water"
-						? "bg-blue-700/50"
-						: query.data?.types[0].type.name === "bug"
-						? "bg-yellow-700/50"
-						: "bg-green-700/50"
-				}`}
+				className={`h-[7rem] w-[13rem]  -bottom-20 left-1/2 -translate-x-1/2 absolute blur-3xl group-hover:h-[20rem] duration-200 ${colors.bgBlur}`}
 			></div>
 		</div>
 	)

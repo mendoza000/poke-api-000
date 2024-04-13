66e8d4b2144bea9ddc226328dcbe4fe2c0dc6c4a
mendoza000 <omarmendoza.alv25@gmail.com>
2024-04-13 17:44:47 -0400

    add size

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,6 @@
 interface Props {
 	children: React.ReactNode
-	size?: "default" | "icon"
+	size?: "default" | "icon" | "small"
 	variant?: "default" | "outline"
 }
 
@@ -12,7 +12,11 @@ export default function Button({
 	return (
 		<button
 			className={`bg-transparent rounded-md py-2 px-5 font-semibold hover:bg-yellow-400/70 duration-200 flex gap-2 hover:gap-3 border-2 border-yellow-400 ${
-				size === "icon" ? "px-3" : "px-5"
+				size === "icon"
+					? "px-3"
+					: size === "small"
+					? "text-sm py-1 px-5"
+					: "px-5"
 			}
     ${
 			variant === "outline"

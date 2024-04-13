c0531b913e8310242e4e6d52b5e74b86cf5f5802
mendoza000 <omarmendoza.alv25@gmail.com>
2024-04-13 10:45:14 -0400

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
 const queryClient = new QueryClient()
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <p>Home</p>,
+	},
+])
 
 function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<ReactQueryDevtools initialIsOpen={false} />
-			<p className="text-4xl">Hola</p>
+			<RouterProvider router={router} />
 		</QueryClientProvider>
 	)
 }

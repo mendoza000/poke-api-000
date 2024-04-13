8cecae20abb6ef173f3b01aa30aa28f629141336
mendoza000 <omarmendoza.alv25@gmail.com>
2024-04-13 12:58:24 -0400

    import font and set home element

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./components/views/home"
+// Supports weights 100-900
+import "@fontsource-variable/chivo"
 
 const queryClient = new QueryClient()
 
 const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <p>Home</p>,
+		element: <Home />,
 	},
 ])
 

1043269344ced65efb0caa47523ebdfdca2aaeea
mendoza000 <omarmendoza.alv25@gmail.com>
2024-04-13 21:07:07 -0400

    create error page

---
+++ src/components/routes/error-page.tsx
@@ -0,0 +1,11 @@
+export default function ErrorPage() {
+	return (
+		<div
+			id="error-page"
+			className="flex flex-col items-center justify-center min-h-screen text-xl text-white"
+		>
+			<h1>Oops!</h1>
+			<p>Sorry, an unexpected error has occurred.</p>
+		</div>
+	)
+}
